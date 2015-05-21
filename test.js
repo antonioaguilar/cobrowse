e0c468877b085ae2d34ea9a0eb110a64bf18efb4
Antonio Aguilar <antonio@macbook-pro.local>
2015-05-21 11:16:59 +0100

    added phantomjs test and fixed travis script

---
+++ test.js
@@ -0,0 +1,23 @@
+// Simple Javascript example
+
+console.log('Loading client web page ...');
+var page = require('webpage').create();
+var url = 'http://localhost:3000/client.html';
+
+page.open(url, function (status) {
+  //Page is loaded!
+  phantom.exit();
+  console.log(status);
+  console.log('Done.')
+});
+
+
+console.log('Loading agent web page ...');
+var page = require('webpage').create();
+var url = 'http://localhost:3000/agent.html';
+
+page.open(url, function (status) {
+  //Page is loaded!
+  phantom.exit();
+  console.log('Done.')
+});
